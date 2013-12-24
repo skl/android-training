211a7903c2b4e2fc767f78270b2af6b36ef2334b
Stephen Lang <info@skl.me>
2013-12-24 22:24:41 +0000

    Adding Action Buttons
    Getting Started > Adding the Action Bar > Adding Action Buttons

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity
 {
@@ -26,5 +29,30 @@ public class MainActivity extends Activity
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+        case R.id.action_search:
+            //openSearch();
+            return true;
+        case R.id.action_settings:
+            //openSettings();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
